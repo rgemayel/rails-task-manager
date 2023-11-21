fc1141acc377f275ec6d70a95a7ce548ed688aa2
Romance Gemayel <25gemayelromance@gmail.com>
2023-11-21 14:00:33 +0200

    add task model

---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ApplicationRecord
+  # def change
+  #   add_column: :tasks, :title, :string
+  #   add_column: :tasks, :details, :text
+  #   add_column: :tasks, :completed, :boolean, default: false
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get "/tasks", to: "tasks#index"
   # root "posts#index"
 end
